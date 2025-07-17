dd2421f0203636feec5b02f11b44c845c80ebd08
BenRyan0 <benryanrinconada5@gmail.com>
2025-07-18 03:56:11 +0800

    resume-preview  -dev0

--- src/pages/MyResume.jsx
+++ src/pages/MyResume.jsx
@@ -84,10 +84,10 @@ const MyResume = () => {
     <div className="h-screen bg-[#020617] flex flex-col justify-center items-center gap-4 pt-6">
       <div
         ref={viewerRef}
-        className={`relative inset-0 w-full md:w-[60%] py-5 rounded-xl overflow-hidden shadow-xl bg-white flex flex-row justify-center items-center text-center  ${
+        className={`relative inset-0 w-full md:w-[60%] py-5 rounded-xl overflow-hidden shadow-xl bg-white flex flex-row justify-center items-center text-center pt-0 ${
           isFullscreen
-            ? "flex justify-center items-center h-screen "
-            : "flex justify-center items-center"
+            ? "flex justify-center items-center text-center h-screen pt-[200px] md:pt-[50px] "
+            : ""
         } cursor-pointer group`}
         onClick={handleFullscreen}
       >
