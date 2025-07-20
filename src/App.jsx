5715a3d473b0df850a81b6641aebb9c6b764e2e8
BenRyan0 <benryanrinconada5@gmail.com>
2025-07-21 04:10:45 +0800

    back-to-basic+model+loadingOpts -dev0

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import MyResume from './pages/MyResume'
+// import Home from './pages/Home'
+// import MyResume from './pages/MyResume'
+
+import { lazy, Suspense } from 'react'
+
+const Home = lazy(() =>import('./pages/Home'))
+const MyResume = lazy(() =>import('./pages/MyResume'))
 
 
 function App() {
   return (
   <Routes>
     <Route path='/' element={<Home />} />
+    {/* <Route path='/resume' element={<Suspense fallback={}/>} /> */}
     <Route path='/resume' element={<MyResume/>} />
   </Routes>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
-import {  useRef } from 'react';
+import { useRef, useEffect, useState } from 'react';
+// import your components
 import NavBar from './../components/NavBar';
 import Hero from './../components/Hero';
 import Hero2 from './../components/Hero2';
@@ -9,29 +10,40 @@ import Projects from './Projects';
 import MyResume from './MyResume';
 
 const Home = () => {
-    const wrapperRef = useRef(null);
-   
+  const wrapperRef = useRef(null);
+  const [visibleCount, setVisibleCount] = useState(1);
+
+  // Sequential reveal
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setVisibleCount((prev) => {
+        if (prev < 7) return prev + 1; // Total of 7 sections
+        clearInterval(interval);
+        return prev;
+      });
+    }, 500); // Time between reveals
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const sectionComponents = [
+    <Hero scrollContainer={wrapperRef} key="hero" />,
+    <Hero2 scrollContainer={wrapperRef} key="hero2" />,
+    <AboutMe key="about" />,
+    <Technologies key="tech" />,
+    <Experience key="exp" />,
+    <Projects key="proj" />,
+    <MyResume key="resume" />
+  ];
+
   return (
-     <div className="flex flex-col items-center justify-center bg-[#020617] ">
-       <div className="container mx-auto px-8 mt-20 lg:mt-0 pb-6 wrapper "   ref={wrapperRef}>
-         <NavBar/>
-          <div id="hero" className='z-10'>
-            <Hero scrollContainer={wrapperRef} />
-          </div>
-          {/* </div> */}
-         <Hero2  scrollContainer={wrapperRef}/>
-         <AboutMe/>
-         <Technologies/>
-         <Experience/>
-         <Projects/>
-         <MyResume/>
-       </div>
-    
+    <div className="flex flex-col items-center justify-center bg-[#020617]">
+      <div className="container mx-auto px-8 mt-20 lg:mt-0 pb-6 wrapper" ref={wrapperRef}>
+        <NavBar />
+        {sectionComponents.slice(0, visibleCount)}
+      </div>
     </div>
+  );
+};
 
-    
-  )
-}
-
-export default Home
-
+export default Home;
